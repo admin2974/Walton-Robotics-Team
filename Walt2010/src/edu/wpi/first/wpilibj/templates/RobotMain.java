af4145d7c7e76d0f4693f3c3fb776d14d98b1e87
kilibob <kilibob@fe68d8d6-c16c-4af0-b3cd-cfa12b6dd21f>
2010-01-28 02:31:32 +0000

    Added TODO notes representing things seen and thought as I wandered through the code.  Refactored attributes and methods for Java naming standards and descriptiveness.  Added/updated method & class-level javadoc where it looked like it could use some.  No functional changes, just the shape and name and lotsa comments.  I'd expect the TODO's to disappear as they either get done or as/if you decide you don't want to look at them anymore!

--- Walt2010/src/edu/wpi/first/wpilibj/templates/RobotMain.java
+++ Walt2010/src/edu/wpi/first/wpilibj/templates/RobotMain.java
@@ -4,9 +4,7 @@
 /* must be accompanied by the FIRST BSD license file in the root directory of */
 /* the project.                                                               */
 /*----------------------------------------------------------------------------*/
-
 package edu.wpi.first.wpilibj.templates;
-
 
 import edu.wpi.first.wpilibj.IterativeRobot;
 import edu.wpi.first.wpilibj.Jaguar;
@@ -19,24 +17,49 @@ import edu.wpi.first.wpilibj.Timer;
  * creating this project, you must also update the manifest file in the resource
  * directory.
  */
-public class RobotMain extends IterativeRobot {
+public class RobotMain extends IterativeRobot
+{
 
+    /**
+     * TODO: Be conscious of attribute scope.  It is convenient when developing
+     * to scope everything in a friendly and accessible fashion, but it can lead
+     * to concurrency issues (among other ill effects) if not managed.  If
+     * multiple threads will be accessing attributes (will they?) then you must
+     * ensure they do so one at a time.  A better strategy might be to scope
+     * private by default:  this will force you to think about how bits of data
+     * are used, which in turn helps concretize the system's structure by
+     * forcing you to think about where info is stored and how it might (or
+     * sometimes must!) flow through your code.  One final thing to research:
+     * is there any advantage in a J2ME environment to allowing direct access
+     * to attributes (versus accessor methods) in terms of execution speed or
+     * active memory overhead?
+     */
     VirtualField field;
+    /**
+     * TODO: This must have concurrency control under the current impl!!
+     * DriveTrain and SpecialJoystick both use this, and not very well! If
+     * SpecialJoystick is to set the afterburner trigger (or any other state
+     * modifier attribute's value) in DriveTrain, consider the different threads
+     * involved in brokering signals from the physical joystick & mechanisms on
+     * the robot - unless I'm very much mistaken you're going to need to
+     * synchronize not only the afterburner set operation but also the reading
+     * of that value within DriveTrain itself.
+     */
     DriveTrain robot_drive;
+    Jaguar leftJag, rightJag;
+    SpecialJoystick leftJoystick, rightJoystick;
 
-    Jaguar jag1, jag2;
-    SpecialJoystick JS1, JS2;
-     /**
+    /**
      * This function is run when the robot is first started up and should be
      * used for any initialization code.
      */
-    public void robotInit()
-    {
-        jag1 = new Jaguar(1);
-        jag2 = new Jaguar(2);
-        JS1 = new SpecialJoystick(this,1);
-        JS2 = new SpecialJoystick(this,2);
-        robot_drive = new DriveTrain(jag1,jag2,JS1,JS2);
+    public void robotInit() {
+        leftJag = new Jaguar(1);
+        rightJag = new Jaguar(2);
+        leftJoystick = new SpecialJoystick(this, 1);
+        rightJoystick = new SpecialJoystick(this, 2);
+        robot_drive = new DriveTrain(leftJag, rightJag, leftJoystick,
+                rightJoystick);
 
         field = new VirtualField();
     }
@@ -45,21 +68,19 @@ public class RobotMain extends IterativeRobot {
      * This function is called periodically during autonomous
      */
     public void autonomousPeriodic() {
-
     }
 
     /**
      * This function is called periodically during operator control
      */
-    public void teleopPeriodic()
-    {
-        while(true)//Infinate loop that checks the state of the robot and driver station every .02 seconds
-        {
+    public void teleopPeriodic() {
+        //Infinite loop that checks the state of the robot and driver station
+        // every .02 seconds
+        while (true) {
             robot_drive.update();
-            JS1.check();
-            JS2.check();
+            leftJoystick.check();
+            rightJoystick.check();
             Timer.delay(kDefaultPeriod);
         }
     }
-    
 }
--- Walt2010/src/edu/wpi/first/wpilibj/templates/SpecialJoystick.java
+++ Walt2010/src/edu/wpi/first/wpilibj/templates/SpecialJoystick.java
@@ -1,39 +1,47 @@
-/*
- * To change this template, choose Tools | Templates
- * and open the template in the editor.
- */
-
 package edu.wpi.first.wpilibj.templates;
 
 import edu.wpi.first.wpilibj.Joystick;
 
 /**
+ * This provides a mechanism for delivering customized functionality based on
+ * joystick inputs.  It relies (read: will need to rely) upon well-defined
+ * contracts between this class and (ideally) the active <code>RobotMain</code>
+ * instance (i.e., the robot out there relying upon this guy to deliver the
+ * goods for him!)
  *
  * @author Admin
  */
 public class SpecialJoystick extends Joystick
 {
-    RobotMain Robot;
-    public SpecialJoystick(RobotMain mainRobot,int channel)
-    {
+
+    /**
+     * TODO:  Consider scope - this ought be private by any metric, neh?
+     */
+    RobotMain robot;
+
+    public SpecialJoystick(RobotMain mainRobot, int channel) {
         super(channel);
-        Robot = mainRobot;
+        robot = mainRobot;
     }
+
     /**
      * This method checks the current state of a joystick and controls the robot accordingly.
      */
-    public void check()
-    {
-        if(this.getTrigger())
-        {
-            Robot.robot_drive.AfterBurners=true;
+    public void check() {
+        //TODO:  Afterburners access must be synchronized!
+        //TODO:  Consider delivering this functionality by defining a method on
+        // RobotMain, perhaps "public void setAfterburners(boolean val)", which
+        // would let you hide the implementation details from this class. The
+        // joystick doesn't (and shouldn't) know that the robot uses a
+        // DriveTrain which has a boolean afterBurners attribute.  This class
+        // should not have to change if the way afterburners are implemented
+        // changes, and a well-defined contract between the joystick and the
+        // robot is the best way to accomplish this here.
+        if (this.getTrigger()) {
+            robot.robot_drive.afterBurners = true;
+        } else {
+            robot.robot_drive.afterBurners = false;
         }
-        else
-        {
-            Robot.robot_drive.AfterBurners=false;
-        }
-            
+
     }
-
-
 }
